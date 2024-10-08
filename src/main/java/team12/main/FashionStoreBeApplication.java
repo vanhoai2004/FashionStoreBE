43d6706006e2b65d63a98d6d50db0dd69928ac75
tranducluong <tranducluong886@gmail.com>
2024-10-08 21:57:41 +0700

    add project

---
+++ src/main/java/team12/main/FashionStoreBeApplication.java
@@ -0,0 +1,13 @@
+package team12.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionStoreBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team12/main/FashionStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package team12.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
